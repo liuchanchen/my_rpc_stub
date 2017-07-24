7e62e1462799f6cd7a476d9db66be6c93dd04b84
剑 刘 <liuchanchen@live.com>
2017-07-24 08:33:05 +0800

    继续增加var，但是会开始在var

--- my_rpc_stub/stub_generator.c
+++ my_rpc_stub/stub_generator.c
@@ -12,16 +12,37 @@
 #define NO_FIND -1
 #define FAILED -1
 
+typedef struct
+{
+	unsigned int current_line_num;
+	unsigned int total_line_num;
+}line_num_t;
+
+typedef enum
+{
+	DSP_STUB=1,
+	ARM_STUB,
+	X_file
+}file_type_e;
+
 const char * file_path = "./dsp_stub.c";
 char para_name[32][8] = { "a0", "a1","a2","a3", "a4", "a5" };
 char *para_name_str = para_name;
 
+static line_num_t dsp_stub_line, arm_stub_line, x_file_line;
+
 static int find_specific_string_in_one_line(unsigned char *str, unsigned char * match_string, unsigned int string_len);
 static int inline match_specific_character(unsigned char letter, unsigned char character);
 static int inline foreach_and_find_specific_character_in_one_line(unsigned char*const str, const unsigned char character);
+static int change_to_another_line(unsigned char * file_content, line_num_t line_num);
 static int find_first_letter_after_specific_string_in_one_line(unsigned char *str, unsigned char * match_string, unsigned int string_len);
 static int parse_var_in_one_line(int write_fd, unsigned char * read_context);
-static int wrap_line(int write_fd);
+static int wrap_line(int write_fd, unsigned int file_type);
+
+static int init_line_num(line_num_t * line_num);
+
+static int add_line_num(line_num_t line_num);
+
 
 int stub_generator(unsigned char*argv)
 {
@@ -32,7 +53,10 @@ int stub_generator(unsigned char*argv)
 	int ret = -1;
 	unsigned int str_len = 0;
 
-	memset(read_context, 0x0, sizeof(read_context));
+	//file line number initialize
+	init_line_num(&dsp_stub_line);
+	init_line_num(&x_file_line);
+	memset(&read_context, 0x0, sizeof(read_context));
 	dsp_stub_fd = open(file_path, O_RDWR | O_CREAT, 0777);
 	if (dsp_stub_fd < 0)
 	{
@@ -54,7 +78,7 @@ int stub_generator(unsigned char*argv)
 	{
 		return -1;
 	}
-	wrap_line(dsp_stub_fd);
+	wrap_line(dsp_stub_fd, DSP_STUB);
 
 	close(dsp_stub_fd);
 	close(interface_file_fd);
@@ -62,7 +86,7 @@ int stub_generator(unsigned char*argv)
 	return 0;
 }
 
-static int parse_var_in_one_line(int write_fd, unsigned char * read_context)
+static int parse_var_in_one_line(int write_fd, unsigned char * read_content)
 {
 	unsigned int str_len = 0;
 	unsigned int read_cnt = 0;
@@ -70,7 +94,7 @@ static int parse_var_in_one_line(int write_fd, unsigned char * read_context)
 
 	str_len = strlen("var");
 	read_cnt = 0;
-	ret = find_specific_string_in_one_line(read_context, "var", str_len);
+	ret = find_specific_string_in_one_line(read_content, "var", str_len);
 	read_cnt = read_cnt + str_len + ret;
 	if (FAILED == ret)
 	{
@@ -78,7 +102,7 @@ static int parse_var_in_one_line(int write_fd, unsigned char * read_context)
 	}
 	printf("read_cnt:%d\n", read_cnt);
 	str_len = strlen("=");
-	ret = find_specific_string_in_one_line(read_context + read_cnt, "=", str_len);
+	ret = find_specific_string_in_one_line(read_content + read_cnt, "=", str_len);
 	read_cnt = read_cnt + str_len + ret;
 	if (FAILED == ret)
 	{
@@ -86,16 +110,21 @@ static int parse_var_in_one_line(int write_fd, unsigned char * read_context)
 	}
 	printf("read_cnt:%d\n", read_cnt);
 	str_len = strlen(";");
-	ret = find_specific_string_in_one_line(read_context + read_cnt, ";", str_len);
+	ret = find_specific_string_in_one_line(read_content + read_cnt, ";", str_len);
 	if (FAILED == ret)
 	{
+		printf("forget ; in line:%d\n", dsp_stub_line.current_line_num);
 		return -1;
 	}
-	write(write_fd, read_context + read_cnt, ret);
+	write(write_fd, read_content + read_cnt, ret);
 	write(write_fd, para_name_str, strlen(para_name_str));
 	para_name_str = para_name_str + sizeof(para_name[0]);
 	write(write_fd, " ;", strlen(" ;"));
 	read_cnt = read_cnt + str_len + ret;
+	ret = change_to_another_line(read_content + read_cnt, dsp_stub_line);
+	//no need adding str_len because ret has been added in change_to_another_line
+	read_cnt = read_cnt + ret;
+	printf("read_cnt:%d\n", read_cnt);
 
 	return read_cnt;
 }
@@ -151,6 +180,25 @@ static int inline foreach_and_find_specific_character_in_one_line(unsigned char*
 	return ret;
 }
 
+static int inline change_to_another_line(unsigned char* file_content, line_num_t line_num)
+{
+	int ret = -1;
+	unsigned int cnt = 0;
+	unsigned char * string = file_content;
+
+	while (NO_FIND == match_specific_character(*file_content, '\n'))
+	{
+		string++;
+		cnt++;
+	}
+	//the index of first character in new line
+	cnt++;
+    //line numeber increase(both total and currnet)
+	add_line_num(line_num);
+
+	return cnt;
+}
+
 static int inline get_line_size(unsigned char*const str)
 {
 	int ret = -1;
@@ -194,9 +242,29 @@ static int find_first_letter_after_specific_string_in_one_line(unsigned char *st
 	return FAILED;
 }
 
-static int wrap_line(int write_fd)
+static int wrap_line(int write_fd, unsigned int file_type)
 {
 	write(write_fd, "\n", 1);
+	if (DSP_STUB == file_type)
+	{
+		dsp_stub_line.total_line_num++;
+	}
+
+	return 0;
+}
+
+static int inline init_line_num(line_num_t * line_num)
+{
+	line_num->current_line_num = 1;
+	line_num->total_line_num = 1;
+
+	return 0;
+}
+
+static int inline add_line_num(line_num_t line_num)
+{
+	line_num.current_line_num++;
+	line_num.total_line_num++;
 
 	return 0;
 }
